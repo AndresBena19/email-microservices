3b863d9c5316b1c268bfea0922665fb320d792bd
andres benavides <andres_felipe16@hotmail.es>
2018-07-06 09:25:33 -0500

    func base view drf

---
+++ micrasApi/recipes/serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from recipes.models import Snippet
+
+
+
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
+
+
+
+
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = UserSerializer()
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
